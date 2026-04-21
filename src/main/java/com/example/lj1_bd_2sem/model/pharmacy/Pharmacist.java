338583f78b7f8582ea74a6ec2768147913491006
rubensvnc <rubensvnc@gmail.com>
2026-04-21 13:27:02 -0300

    feat: add medicine search

--- src/main/java/com/example/lj1_bd_2sem/model/pharmacy/Pharmacist.java
+++ src/main/java/com/example/lj1_bd_2sem/model/pharmacy/Pharmacist.java
@@ -1,9 +1,20 @@
 package com.example.lj1_bd_2sem.model.pharmacy;
 
+import com.example.lj1_bd_2sem.model.common.Business;
 import com.example.lj1_bd_2sem.model.common.Employee;
+import com.example.lj1_bd_2sem.model.common.Product;
 
 public class Pharmacist extends Employee {
     public Pharmacist(String cpf, Integer idade, String name){
         super(cpf, idade, name);
+    }
+
+    public Medicine searchMedicine(String name, Pharmacy pharmacy){
+        Medicine medicine = pharmacy.searchStorage(name);
+        if (medicine != null){
+            return medicine;
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/com/example/lj1_bd_2sem/model/pharmacy/Pharmacy.java
+++ src/main/java/com/example/lj1_bd_2sem/model/pharmacy/Pharmacy.java
@@ -4,10 +4,22 @@ import com.example.lj1_bd_2sem.model.common.Business;
 import com.example.lj1_bd_2sem.model.common.Employee;
 import com.example.lj1_bd_2sem.model.common.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pharmacy extends Business {
+    private List<Medicine> medicines = new ArrayList<>();
+
     public Pharmacy (String cnpj){
         super(cnpj);
+    }
+
+    public Medicine searchStorage(String name){
+        for (Medicine medicine : medicines){
+            if (medicine.getName().equals(name)){
+                return medicine;
+            }
+        }
+        return null;
     }
 }
